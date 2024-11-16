73d4ca8eafb4090bc3133071ccfcad45d5bf2a09
gayatri023 <113886941+gayatri023@users.noreply.github.com>
2024-11-16 07:06:21 +0000

    dataTypes

---
+++ js_basics/2_datatype.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello, world !")  // can't use as we are using node js and not browser
+
+console.log("nikki")
+
+let name = " Ankii" 
+let age = 35     // number => 2 power 53  (range) 
+let isLoggedIn = false
+
+/*
+bigint - used mostly in large number like stock market or big websites like reddit and facebook 
+string => "  "
+boolean => true/false
+null => standout alone
+undefined
+symbol => when create many component - to define uniqueness - we use symbol
+
+oject - cover in detailed
+*/
+
+console.log(typeof age);
+console.log(typeof null);    // output is object
+console.log(typeof undefined); //output is undefined
+
