a99979e39aa9597215953df15ac02fcc7cc0696e
gayatri023 <113886941+gayatri023@users.noreply.github.com>
2026-01-15 00:17:53 +0530

    Update 3_conversionOp.js

--- js_basics/3_conversionOp.js
+++ js_basics/3_conversionOp.js
@@ -46,3 +46,43 @@ let someNumber = 33
 let stringNumber = String(someNumber)             // number
 console.log(stringNumber);
 console.log(typeof stringNumber);                 // string
+
+
+lst value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = "hitesh"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);   // 12
+console.log(1 + "2");   // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32  - why happened understand in ECMA
+
+// its better to use parenthesis and code make it simple and clean
+
+console.log(+ true);   // 1 --- true is bol can not be incremented so give the real value
+console.log(+ "");     // 0 
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2;
+
+// post fix and pre fix -- incremental component
+let gameCounter = 100
+++gmaeCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
+
+
+
+
