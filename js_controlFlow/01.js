b30399629136793b742f3edb7801c073a1584d86
gayatri023 <113886941+gayatri023@users.noreply.github.com>
2026-01-20 02:54:47 +0530

    Update 01.js

--- js_controlFlow/01.js
+++ js_controlFlow/01.js
@@ -7,10 +7,18 @@ else{
     console.log("temperature is more than 50");
 }
 
+const score = 200
+
+if (score > 100) {
+    let power = "fly"
+    console.log(`User power: $(power)`);
+
 const balance = 1000
-// if(balance > 500) console.log("test"), not a good practices - immature code
+// if(balance > 500) console.log("test"),console.log("test2")' not a good practices - immature code
 if (balance < 500){
     console.log("less than 500");    
+}else if (balance < 750) {
+    console.log("less than 750");
 }else if (balance < 900){
     console.log("less than 700"); 
 } else {
