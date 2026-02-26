fe5c2a2f69a91a66521d905e70ee57aecf0e6842
gayatri023 <113886941+gayatri023@users.noreply.github.com>
2026-02-26 18:19:29 +0530

    Update 7.js

--- JS /05_iterations/7.js
+++ JS /05_iterations/7.js
@@ -1,10 +1,11 @@
+// map - transforms every element and returns a new array. It does not change the original array.
 const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // const newNums = myNumers.map( (num) => { return num + 10})
 
 const newNums = myNumers
-                .map((num) => num * 10 )
-                .map( (num) => num + 1)
-                .filter( (num) => num >= 40)
+                .map((num) => num * 10 )      //[10,20,30,40,50,60,70,80,90,100]
+                .map((num) => num + 1)        //[11,21,31,41,51,61,71,81,91,101]
+                .filter((num) => num >= 40)  // [41,51,61,71,81,91,101]
 
 console.log(newNums);
